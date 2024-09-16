d9ec5344240189e2ccff8ad7c61eca38cc0aaf7a
Jack Coutts <jackcoutts@Jacks-MacBook-Pro.local>
2024-09-16 11:38:31 +0100

    repo init

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import argparse
+from subprocess import run
+
+"""Constants"""
+default_ensembl_ftp_url = (
+    "ftp://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-"
+)
+temp_download_folder = os.path.abspath(
+    os.path.expanduser("~/temp-ftp-downloads/")
+)
+
+
+def download_gtf_files(release: str | int, species: str):
+    """
+    Download GTF files from Ensembl FTP server
+
+    Args:
+        release: Ensembl release version
+        species: Ensembl species name
+
+    Returns:
+        None
+        * Downloads GTF files to temp_download_folder
+
+    """
+
+    wget_command = [
+        "wget",
+        "-r",  # recursive
+        "-l",
+        "1",  # limit recursion depth to 1
+        "-np",  # don't ascend to the parent directory
+        "-nd",  # don't recreate directory structure
+        "--directory-prefix",
+        temp_download_folder,
+        "-A",
+        "*.gtf.gz",  # accept only .gtf.gz files
+        "-R",
+        "*.chr*,*abinitio*",  # reject files containing matching patterns
+        f"{default_ensembl_ftp_url}{release}/gtf/{species}/",
+    ]
+    result = run(wget_command, capture_output=True, text=True)
+
+    if result.returncode == 0:
+        print(f"Successfully downloaded to {temp_download_folder}")
+    else:
+        print(f"Error occurred: {result.stderr}")
+
+
+def unzip_gtf_files():
+
+    return
+
+
+def main():
+    parser = argparse.ArgumentParser(description="tbd")
+    parser.add_argument("--species", type=str, help="Ensembl species name")
+    parser.add_argument("--release", type=int, help="Ensembl release version")
+    args = parser.parse_args()
+
+    download_gtf_files(args.release, args.species)
+
+
+if __name__ == "__main__":
+    main()
